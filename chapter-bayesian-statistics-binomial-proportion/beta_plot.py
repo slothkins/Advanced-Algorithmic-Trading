d1866afad906329c42b6f3d2d092476aba665232
slothkins <nicolas.atkins@me.com>
2021-07-09 10:55:59 +0100

    9 july office

--- chapter-bayesian-statistics-binomial-proportion/beta_plot.py
+++ chapter-bayesian-statistics-binomial-proportion/beta_plot.py
@@ -8,7 +8,7 @@ import seaborn as sns
 
 if __name__ == "__main__":
     sns.set_palette("deep", desat=.6)
-    sns.set_context(rc={"figure.figsize": (8, 4)})
+    # sns.set_context(rc={"figure.figsize": (8, 4)})
     x = np.linspace(0, 1, 100)
     params = [
         (0.5, 0.5),
--- chapter-bayesian-statistics-markov-chain-monte-carlo/bayes-binomial-mcmc.py
+++ chapter-bayesian-statistics-markov-chain-monte-carlo/bayes-binomial-mcmc.py
@@ -3,64 +3,66 @@ import numpy as np
 import pymc3
 import scipy.stats as stats
 
-plt.style.use("ggplot")
 
-# Parameter values for prior and analytic posterior
-n = 50
-z = 10
-alpha = 12
-beta = 12
-alpha_post = 22
-beta_post = 52
+if __name__ == "__main__":
+    plt.style.use("ggplot")
 
-# How many samples to carry out for MCMC
-iterations = 100000
+    # Parameter values for prior and analytic posterior
+    n = 50
+    z = 10
+    alpha = 12
+    beta = 12
+    alpha_post = 22
+    beta_post = 52
 
-# Use PyMC3 to construct a model context
-basic_model = pymc3.Model()
-with basic_model:
-    # Define our prior belief about the fairness
-    # of the coin using a Beta distribution
-    theta = pymc3.Beta("theta", alpha=alpha, beta=beta)
+    # How many samples to carry out for MCMC
+    iterations = 100000
 
-    # Define the Bernoulli likelihood function
-    y = pymc3.Binomial("y", n=n, p=theta, observed=z)
+    # Use PyMC3 to construct a model context
+    basic_model = pymc3.Model()
+    with basic_model:
+        # Define our prior belief about the fairness
+        # of the coin using a Beta distribution
+        theta = pymc3.Beta("theta", alpha=alpha, beta=beta)
 
-    # Carry out the MCMC analysis using the Metropolis algorithm
-    # Use Maximum A Posteriori (MAP) optimisation as initial value for MCMC
-    start = pymc3.find_MAP() 
+        # Define the Bernoulli likelihood function
+        y = pymc3.Binomial("y", n=n, p=theta, observed=z)
 
-    # Use the Metropolis algorithm (as opposed to NUTS or HMC, etc.)
-    step = pymc3.Metropolis()
+        # Carry out the MCMC analysis using the Metropolis algorithm
+        # Use Maximum A Posteriori (MAP) optimisation as initial value for MCMC
+        start = pymc3.find_MAP()
 
-    # Calculate the trace
-    trace = pymc3.sample(iterations, step, start, random_seed=1, progressbar=True)
+        # Use the Metropolis algorithm (as opposed to NUTS or HMC, etc.)
+        step = pymc3.Metropolis()
 
-# Plot the posterior histogram from MCMC analysis
-bins=50
-plt.hist(
-    trace["theta"], bins, 
-    histtype="step", normed=True, 
-    label="Posterior (MCMC)", color="red"
-)
+        # Calculate the trace
+        trace = pymc3.sample(iterations, step, start, random_seed=1, progressbar=True)
 
-# Plot the analytic prior and posterior beta distributions
-x = np.linspace(0, 1, 100)
-plt.plot(
-    x, stats.beta.pdf(x, alpha, beta), 
-    "--", label="Prior", color="blue"
-)
-plt.plot(
-    x, stats.beta.pdf(x, alpha_post, beta_post), 
-    label='Posterior (Analytic)', color="green"
-)
+    # Plot the posterior histogram from MCMC analysis
+    bins=50
+    plt.hist(
+        trace["theta"], bins,
+        histtype="step", density=True,
+        label="Posterior (MCMC)", color="red"
+    )
 
-# Update the graph labels
-plt.legend(title="Parameters", loc="best")
-plt.xlabel("$\\theta$, Fairness")
-plt.ylabel("Density")
-plt.show()
+    # Plot the analytic prior and posterior beta distributions
+    x = np.linspace(0, 1, 100)
+    plt.plot(
+        x, stats.beta.pdf(x, alpha, beta),
+        "--", label="Prior", color="blue"
+    )
+    plt.plot(
+        x, stats.beta.pdf(x, alpha_post, beta_post),
+        label='Posterior (Analytic)', color="green"
+    )
 
-# Show the trace plot
-pymc3.traceplot(trace)
-plt.show()
+    # Update the graph labels
+    plt.legend(title="Parameters", loc="best")
+    plt.xlabel("$\\theta$, Fairness")
+    plt.ylabel("Density")
+    plt.show()
+
+    # Show the trace plot
+    pymc3.traceplot(trace)
+    plt.show()
